30c860da91105d265fb17e5c24c21f1b4320106e
Axsiow <TehSiow2@gmail.com>
2023-05-07 18:43:39 +0200

    a

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client({intents: 3276799});
+const load_commands = require('./loader/load_commands');
 const config = require('./config');
 
-client.login(config.token);
+bot.commands = new Discord.Collection();
 
-client.on('ready', async () => {
-	console.log("L'œil vous regarde.");
-	console.log(`Logged in as ${client.user.tag}!`);
+bot.login(config.token);
+load_commands(bot);
+
+bot.on('messageCreate', async message => {
+	if(message.content === "!ping") return bot.commands.get('ping').run(bot, message)
 });
 
+bot.on('ready', async () => {
+	console.log("L'œil vous regarde.");
+	console.log(`Logged in as ${bot.user.tag}!`);
+})
