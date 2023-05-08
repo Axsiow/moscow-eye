931d1f680c6d8b51b46c3197edcc24f7c3a39626
Axsiow <TehSiow2@gmail.com>
2023-05-08 17:49:38 +0200

    a

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client({intents: 3276799});
-const load_commands = require('./loaders/load_commands');
-const load_events = require('./loaders/load_events');
-const config = require('./config');
+const { Client, Collection} = require('discord.js');
+const { token } = require('./config.json');
+const client = new Client({intents: 3276799});
 
-bot.commands = new Discord.Collection();
+client.commands = new Collection();
 
-bot.login(config.token);
-load_commands(bot);
-load_events(bot);
-
-bot.on('messageCreate', async message => {
-	if(message.content === "!ping") return bot.commands.get('ping').run(bot, message)
+["commands"].forEach((handler) => {
+	require(`./handlers/${handler}`);
 });
+
+client.on('ready', () => {
+	console.log(`${client.user.tag} vous regarde.`);
+});
+
+client.login(token);
