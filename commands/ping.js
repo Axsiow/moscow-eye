b1b7b3eedba5cdb27f0c370f24f1677bd9aceccb
Axsiow <TehSiow2@gmail.com>
2023-05-08 20:15:33 +0200

    a

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
 	name: "ping",
 	description: "show ping of bot",
-	permission: "NONE",
+	permission: "None",
 	dm: true,
 
 	async run(bot, message) {
